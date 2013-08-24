d486a80bb7c638ef5b9f6ce50a0a199367f2c863
Tobias Rehbein <tobias.rehbein@web.de>
2013-08-24 17:46:42 +0200

    Add an example for timer interrupts

---
+++ blink-interrupt/main.c
@@ -0,0 +1,40 @@
+#define F_CPU 1000000UL
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdint.h>
+
+#define LED1	(1<<PD5)
+#define LED2	(1<<PD6)
+
+#define TIMER_PRESCALE_8 (1<<CS11)
+
+ISR(TIMER1_OVF_vect)
+{
+	PORTD ^= LED1 | LED2;
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+	PORTD ^= LED2;
+}
+
+int main(void)
+{
+	/* outputs */
+	DDRD |= LED1 | LED2;
+
+	/* set timer */
+	TCCR1B |= TIMER_PRESCALE_8;
+
+	OCR1A = (65535 / 2);
+
+	/* enable interrupts */
+	TIMSK |= (1<<TOIE1) | (1<<OCIE1A);
+	sei();
+
+	for (;;);
+
+	/* NOTREACHED */
+	return (0);
+}
